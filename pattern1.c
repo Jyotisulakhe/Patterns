28228ca634553cc34b30342f7d3f71090383bbfe
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-03-19 12:15:27 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void numCount();
+int main()
+{
+    numCount();//function for use user.
+
+    return 0;
+}
+void numCount()
+{
+    for(int i=1;i<=5;i++)//row
+    {
+     for(int j=1;j<=i;j++)//column
+
+      printf("*");
+         //printf("  ");
+         printf("\n");
+         }
+         }
+
+
+
